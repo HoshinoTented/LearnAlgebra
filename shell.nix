2c08790c0dff79af69b1f008500cf38476a46673
Hoshino Tented <hoshinotented@qq.com>
2025-08-04 01:00:41 +0000

    now linear

---
+++ shell.nix
@@ -0,0 +1,16 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/50ab793786d9de88ee30ec4e4c24fb4236fc2674";
+  pkgs = import nixpkgs {};
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    (texlive.combine {
+      inherit (texlive)
+        scheme-medium
+        cleveref
+        subfiles
+        ;
+    })
+  ];
+}
