49231a1de7794db3b8650bae622b4f99d06da0e8
Hoshino Tented <hoshinotented@qq.com>
2025-08-28 17:33:42 +0000

    update

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/50ab793786d9de88ee30ec4e4c24fb4236fc2674";
-  pkgs = import nixpkgs {};
+  # nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/50ab793786d9de88ee30ec4e4c24fb4236fc2674";
+  pkgs = import <nixpkgs> {};
 in
 
 pkgs.mkShellNoCC {
